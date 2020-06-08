4a819902b3cba780e7ed333f39e759bfb5c38f36
DanielGeek <daniel30081990@gmail.com>
2020-06-08 16:47:58 -0400

    instalado react-router-dom en el client

--- web-personal/client/src/App.js
+++ web-personal/client/src/App.js
@@ -1,23 +1,12 @@
 import React from 'react';
-import { DatePicker, Card } from 'antd';
+
 import './App.scss';
 
 function App() {
 
-  const test = (date, dateString) => {
-    console.log(date, dateString);
-  };
-
   return (
     <div className="app">
-      <h1>Web personal - client</h1>
-      <h2>Proyecto</h2>
-      <DatePicker onChange={test} />
       
-      <Card title="Test..." style={{ width: 300 }}>
-      <p>Hola mundo...</p>
-      
-    </Card>
     </div>
   );
 }

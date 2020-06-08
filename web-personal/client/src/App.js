997a607475b70efa8c2fc0155ef9497024869dc6
DanielGeek <daniel30081990@gmail.com>
2020-06-08 12:11:56 -0400

    Añadiendo SASS al proyecto

--- web-personal/client/src/App.js
+++ web-personal/client/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <h1>Web personal - client</h1>
+      <h2>Proyecto</h2>
     </div>
   );
 }

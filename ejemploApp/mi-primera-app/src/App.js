66967ee05ea27267d657ab739691979ecc17d112
DanielGeek <daniel30081990@gmail.com>
2020-06-08 11:06:26 -0400

    Template Strings

--- ejemploApp/mi-primera-app/src/App.js
+++ ejemploApp/mi-primera-app/src/App.js
@@ -7,24 +7,21 @@ import Saludar from "./components/Saludar";
 
 function App() {
   const user = {
-    // nombre: "Daniel Ángel",
+    nombre: "Daniel Ángel",
     edad:29,
     color: "Rojo"
   };
 
-  const saludarFn = name => {
-    console.log("Hola " + name);
+  const saludarFn = (name, edad) => {
+    console.log("Hola " + name + " tiene " + edad + " años.");
+    console.log(`Hola ${nombre}, tiene ${edad} años.`);
   };
-
-  const dioBlur = () => {
-    alert('on blur!');
-  }
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Saludar userInfo={user} saludarFn={saludarFn} dioBlur={dioBlur} />
+        <Saludar userInfo={user} saludarFn={saludarFn} />
       </header>
     </div>
   );
--- ejemploApp/mi-primera-app/src/components/Saludar.js
+++ ejemploApp/mi-primera-app/src/components/Saludar.js
@@ -1,13 +1,13 @@
 import React from "react";
 
 export default function Saludar(props) {
-    const { userInfo, saludarFn, dioBlur } = props;
-    const { nombre = "Anonimo" } = userInfo;
+    const { userInfo, saludarFn } = props;
+    const { nombre = "Anonimo", edad} = userInfo;
     console.log(props);
     console.log(userInfo)
     return (
         <div>
-            <button onClick={() => saludarFn(nombre)}>Saludar</button>
+            <button onClick={() => saludarFn(nombre, edad)}>Saludar</button>
         </div>
     )
 }

10b771110a299f7abcdbd49bfcf1f7d0ac1e25b7
DanielGeek <daniel30081990@gmail.com>
2020-06-08 12:31:55 -0400

    Añadiendo la librería Ant Design al proyecto

--- web-personal/client/src/App.js
+++ web-personal/client/src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import { DatePicker, Card } from 'antd';
 import './App.scss';
 
 function App() {
+
+  const test = (date, dateString) => {
+    console.log(date, dateString);
+  };
+
   return (
     <div className="app">
       <h1>Web personal - client</h1>
       <h2>Proyecto</h2>
+      <DatePicker onChange={test} />
+      
+      <Card title="Test..." style={{ width: 300 }}>
+      <p>Hola mundo...</p>
+      
+    </Card>
     </div>
   );
 }

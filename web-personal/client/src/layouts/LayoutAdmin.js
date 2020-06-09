a5e868b681e93a4a9b9cf9ffe0945a44721a9b17
DanielGeek <daniel30081990@gmail.com>
2020-06-09 14:39:40 -0400

    Dando estilos al LayoutAdmin

--- web-personal/client/src/layouts/LayoutAdmin.js
+++ web-personal/client/src/layouts/LayoutAdmin.js
@@ -11,13 +11,15 @@ export default function LayoutAdmin(props) {
 
   return (
     <Layout>
-      <h2>Menu Sider Admin</h2>
-      <Layout>
-        <Header>Header...</Header>
-        <Content>
+      { /* TO DO: Menu Sider */}
+      <Layout className="layout-admin">
+        <Header className="layout-admin__header">
+        { /* TO DO: Menu Sider */}
+        </Header>
+        <Content className="layout-admin__content">
           <LoadRoutes routes={routes} />
         </Content>
-        <Footer>Daniel Angel</Footer>
+        <Footer className="layout-admin__footer">Daniel Angel</Footer>
       </Layout>
     </Layout>
   );

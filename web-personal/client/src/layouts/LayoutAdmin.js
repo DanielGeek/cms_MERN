916d2b303ab15f6d9daa18c05b070e02dcb8dfbd
DanielGeek <daniel30081990@gmail.com>
2020-06-08 21:16:53 -0400

    Programando el sistema de rutas para cargar las páginas dentro del LayoutAdmin

--- web-personal/client/src/layouts/LayoutAdmin.js
+++ web-personal/client/src/layouts/LayoutAdmin.js
@@ -1,10 +1,37 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import { Layout } from "antd";
 
-export default function LayoutAdmin() {
-  return <Layout>
+import "./LayoutAdmin.scss";
+
+export default function LayoutAdmin(props) {
+  const { routes } = props;
+  const { Header, Content, Footer } = Layout;
+  // console.log(props);
+
+  return (
+    <Layout>
       <h2>Menu Sider Admin</h2>
-      <div>Contenido....</div>
-      <h5>Footer...</h5>
-  </Layout>;
+      <Layout>
+        <Header>Header...</Header>
+        <Content>
+          <LoadRouters routes={routes} />
+        </Content>
+        <Footer>Daniel Angel</Footer>
+      </Layout>
+    </Layout>
+  );
+}
+
+function LoadRouters({ routes }) {
+  console.log(routes);
+
+  return routes.map((route, index) => (
+    <Route
+      key={index}
+      path={route.path}
+      exact={route.exact}
+      component={route.component}
+    />
+  ));
 }

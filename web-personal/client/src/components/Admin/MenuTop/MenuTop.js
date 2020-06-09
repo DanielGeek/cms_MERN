51274be0ce6e73be9073de9eeb7d185a9e086a75
DanielGeek <daniel30081990@gmail.com>
2020-06-09 16:39:19 -0400

    Centrando Contenido y añadiendo funcionalidad al botón de abrir el MenuSider

--- web-personal/client/src/components/Admin/MenuTop/MenuTop.js
+++ web-personal/client/src/components/Admin/MenuTop/MenuTop.js
@@ -4,17 +4,19 @@ import Logo from "../../../assets/img/png/logo-white3.png";
 
 import "./MenuTop.scss";
 
-export default function MenuTop() {
+export default function MenuTop(props) {
+  const { menuCollapsed, setMenuCollapsed } = props;
+
   return (
     <div className="menu-top">
       <div className="menu-top__left">
         <img className="menu-top__left-logo" src={Logo} alt="logo" />
-        <Button type="link" onClick={() => console.log("Click.")}>
-          <Icon type="menu-fold" />
+        <Button type="link" onClick={() => setMenuCollapsed(!menuCollapsed)}>
+          <Icon type={menuCollapsed ? "menu-unfold" : "menu-fold"} />
         </Button>
       </div>
       <div className="menu-top__right">
-        <Button type="link" onClick={() => console.log('desconexion...')}>
+        <Button type="link" onClick={() => console.log("desconexion...")}>
           <Icon type="poweroff" />
         </Button>
       </div>

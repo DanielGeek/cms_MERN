396dbcd3cac904a02577afd5ef107be4d81e5482
DanielGeek <daniel30081990@gmail.com>
2020-06-21 20:29:29 -0400

    Reutilizando las funciónes de validación en el formulario de registro de usuario

--- web-personal/client/src/components/Admin/RegisterForm/RegisterForm.js
+++ web-personal/client/src/components/Admin/RegisterForm/RegisterForm.js
@@ -1,78 +1,121 @@
 import React, { useState } from "react";
 import { Form, Icon, Input, Button, Checkbox, notificacion } from "antd";
+import {
+  emailValidation,
+  minLengthValidation
+} from "../../../utils/formValidation";
 
 import "./RegisterForm.scss";
 
 export default function RegisterForm() {
-    const [inputs, setInputs] = useState({
+  const [inputs, setInputs] = useState({
     email: "",
     password: "",
     repeatPassword: "",
     privacyPolicy: false
-    });
+  });
 
-    const changeForm = e => {
-        if (e.target.name === "privacyPolicy") {
-            setInputs({
-                ...inputs,
-                [e.target.name]: e.target.checked
-            });
-        } else {
-            setInputs({
-                ...inputs,
-                [e.target.name]: e.target.value
-            });
-        }
-    };
+  const [formValid, setFormValid] = useState({
+    email: false,
+    password: false,
+    repeatPassword: false,
+    privacyPolicy: false
+  });
 
-    const register = (e) => {
-        e.preventDefault();
-        console.log(inputs);
-    };
+  const changeForm = e => {
+    if (e.target.name === "privacyPolicy") {
+      setInputs({
+        ...inputs,
+        [e.target.name]: e.target.checked
+      });
+    } else {
+      setInputs({
+        ...inputs,
+        [e.target.name]: e.target.value
+      });
+    }
+  };
 
-    return (
-        <Form className="register-form" onSubmit={register} onChange={changeForm}>
-            <Form.Item>
-            <Input
-                prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25"}} />}
-                type="email"
-                name="email"
-                placeholder="Correo electronico"
-                className="reigster-form__input"
-                value={inputs.email}
-             />
-            </Form.Item>
-            <Form.Item>
-            <Input
-                prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25"}} />}
-                type="password"
-                name="password"
-                placeholder="Contraseña"
-                className="reigster-form__input"
-                value={inputs.password}
-             />
-            </Form.Item>
-            <Form.Item>
-            <Input
-                prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25"}} />}
-                type="password"
-                name="repeatPassword"
-                placeholder="Repetir contraseña"
-                className="reigster-form__input"
-                value={inputs.repeatPassword}
-             />
-            </Form.Item>
-            <Form.Item>
-            <Checkbox name="privacyPolicy" checked={inputs.privacyPolicy} >
-                He leído y acepto la política de privacidad.
-            </Checkbox>
-            </Form.Item>
+  const inputValidation = e => {
+      const { type, name } = e.target;
+    
+      if(type === "email") {
+          setFormValid({
+              ...formValid,
+              [name]: emailValidation(e.target)
+          });
+      }
 
-            <Form.Item>
-            <Button htmlType="submit" className="register-form__button">
-                Crear cuenta
-            </Button>
-            </Form.Item>
-        </Form>
-    )
+      if(type === "password") {
+        setFormValid({
+            ...formValid,
+            [name]: emailValidation(e.target, 6)
+        });
+    }
+
+    if(type === "checkbox") {
+        setFormValid({
+            ...formValid,
+            [name]: e.target.checked
+        });
+    }
+  };
+
+  const register = e => {
+    e.preventDefault();
+    console.log(formValid);
+  };
+
+  return (
+    <Form className="register-form" onSubmit={register} onChange={changeForm}>
+      <Form.Item>
+        <Input
+          prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25" }} />}
+          type="email"
+          name="email"
+          placeholder="Correo electronico"
+          className="reigster-form__input"
+          onChange={inputValidation}
+          value={inputs.email}
+        />
+      </Form.Item>
+      <Form.Item>
+        <Input
+          prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25" }} />}
+          type="password"
+          name="password"
+          placeholder="Contraseña"
+          className="reigster-form__input"
+          onChange={inputValidation}
+          value={inputs.password}
+        />
+      </Form.Item>
+      <Form.Item>
+        <Input
+          prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25" }} />}
+          type="password"
+          name="repeatPassword"
+          placeholder="Repetir contraseña"
+          className="reigster-form__input"
+          onChange={inputValidation}
+          value={inputs.repeatPassword}
+        />
+      </Form.Item>
+      <Form.Item>
+        <Checkbox
+          name="privacyPolicy"
+          onChange={inputValidation}
+          checked={inputs.privacyPolicy}
+        >
+          He leído y acepto la política de privacidad.
+        </Checkbox>
+      </Form.Item>
+
+      <Form.Item>
+        <Button htmlType="submit" className="register-form__button">
+          Crear cuenta
+        </Button>
+      </Form.Item>
+    </Form>
+  );
 }
